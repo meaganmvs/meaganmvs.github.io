e4030b2905a35962cb6ef39e56fde04991d121f1
meaganmvs <meagansievers@gmail.com>
2018-08-05 13:54:45 -0400

    Adding grunt

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      concat: {
+          options: {
+            separator: ';'
+        },
+        dist: {
+            src: ['src/**/*.js'],
+            dest: 'dist/<%= pkg.name %>.js'
+          }
+      },
+      uglify: {
+        options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+          },
+          dist: {
+            files: {
+              'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+            }
+          }  
+      },
+      buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:meaganmvs/meganmvs.github.io.git',
+          branch: 'gh-pages'
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
+      }
+    },
+
+  });
+
+    
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-build-control');
+grunt.registerTask('default', ['concat', 'uglify', 'buildcontrol']);
+
+
+};
